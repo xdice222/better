dcab4fe07b9c46594f7b4361427617bef0793fa8
ervin_beñez <ervinbenez@gmail.com>
2024-02-19 11:02:59 +0800

    css fixed

--- 02_better/public/products/index.php
+++ 02_better/public/products/index.php
@@ -1,3 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link href="app.css" rel="stylesheet">
+</head>
+<body>
 <?php
 
 
@@ -69,4 +78,6 @@ $products = $statement->fetchAll(PDO::FETCH_ASSOC);
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
   </body>
+</html>
+</body>
 </html>
